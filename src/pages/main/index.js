4da8b22f89b034544154581a5357f0d01ed940c3
Leandro <leandrodbdias@gmail.com>
2020-04-14 20:35:06 -0300

    Estrutura base para criação do game

---
+++ src/pages/main/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Component } from "react";
+
+import "./style.css";
+
+export default class Main extends Component {
+  render() {
+    return <h1>Olá Pokemons</h1>;
+  }
+}
